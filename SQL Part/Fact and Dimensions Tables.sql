078dec0ae9020d474d71687ac18922cb9984bdf1
Amandeep Singh <149965142+amandeep-singh28@users.noreply.github.com>
2025-07-09 22:29:24 +0530

    Add files via upload

---
+++ SQL Part/Fact and Dimensions Tables.sql
@@ -0,0 +1,10 @@
+USE project1;
+
+-- Checking if data is loaded correctly or not...?
+SELECT * FROM fact_table;
+
+SELECT * FROM character_table;
+
+SELECT * FROM genre_table;
+
+SELECT * FROM title_table;
